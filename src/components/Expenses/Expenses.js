f11a7ca4fa87dbd3c0cdf5b9177c9525f9593e6f
Jurgita <jurgita.eicinaite@yahoo.com>
2023-02-22 10:38:19 +0200

    add new expense button, form showing conditionally

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,8 @@
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import { useState } from 'react';
-
+import ExpensesList from './ExpensesList';
 
 function Expenses({ expenses }) {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -12,13 +11,17 @@ function Expenses({ expenses }) {
         setFilteredYear(year);
     };
 
-    return (
-        <Card className='expenses'>
-            <ExpensesFilter onFilter={filterHandler} selected={filteredYear} />
-            {expenses.map(e => <ExpenseItem key={e.id} title={e.title} amount={e.amount} date={e.date} />)}
-        </Card>
+    const filteredExpenses = expenses.filter((e) => e.date.getFullYear().toString() === filteredYear);
 
+    return (
+        <Card className="expenses">
+            <ExpensesFilter
+                onFilter={filterHandler}
+                selected={filteredYear}
+            />
+            <ExpensesList items={filteredExpenses} />
+        </Card>
     );
-};
+}
 
 export default Expenses;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,33 @@
-import React from "react";
-import ExpenseForm from "./ExpenseForm";
+import React, { useState } from 'react';
+import ExpenseForm from './ExpenseForm';
 
 import './NewExpense.css';
 
 const NewExpense = ({ onAddExpense }) => {
+    const [isEditting, setIsEditting] = useState(false);
+    const startEdittingHandler = () => {
+        setIsEditting(true);
+    };
+    const stopEdditingHandler = () => setIsEditting(false);
+
     const saveExpenseDataHandler = (eneterdExpenseData) => {
         const expenseData = {
             ...eneterdExpenseData,
-            id: Math.random().toString()
+            id: Math.random().toString(),
         };
         onAddExpense(expenseData);
+        setIsEditting(false);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {!isEditting && <button onClick={startEdittingHandler}>Add New Expense</button>}
+            {isEditting && (
+                <ExpenseForm
+                    onSaveExpenseData={saveExpenseDataHandler}
+                    onCancel={stopEdditingHandler}
+                />
+            )}
         </div>
     );
 };
